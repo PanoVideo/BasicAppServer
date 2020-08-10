871b68a77f717e7888ad6b8924419bcb00067ad1
“kevin” <kevinruan@pano.im>
2020-08-10 10:25:22 +0800

    init BasicAppServer for Java and Go

---
+++ Go/app_server.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type AppLoginRequest struct {
+	AppId           string `json:"appId" binding:"required"`
+	ChannelId       string `json:"ChannelId" binding:"required"`
+	UserId          string `json:"UserId"`
+	Privileges      uint   `json:"Privileges"`
+	Duration        uint   `json:"Duration"`
+	ChannelDuration uint   `json:"ChannelDuration"`
+}
+
+type PanoTokenRequest struct {
+	ChannelId       string `json:"channelId" binding:"required"`
+	UserId          string `json:"userId"`
+	Privileges      uint   `json:"privileges"`
+	Duration        uint   `json:"duration"`
+	ChannelDuration uint   `json:"channelDuration"`
+}
+
+type SdkToken struct {
+	Token string `json:token`
+}
+
+func main() {
+	//put your APP_SECRET here
+	const APP_SECRET = "xxx"
+
+	const PANO_TOKEN_URL = "https://api.pano.video/auth/token"
+
+	router := gin.Default()
+
+	router.POST("/app/login", func(c *gin.Context) {
+
+		var req AppLoginRequest
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if req.Duration == 0 {
+			req.Duration = 1234
+		}
+		if req.ChannelDuration == 0 {
+			req.ChannelDuration = 60
+		}
+		if req.UserId == "" {
+			req.UserId = "20190822"
+		}
+
+		postBody := PanoTokenRequest{
+			ChannelId:       req.ChannelId,
+			UserId:          req.UserId,
+			Privileges:      req.Privileges,
+			Duration:        req.Duration,
+			ChannelDuration: req.ChannelDuration,
+		}
+		b, err := json.Marshal(postBody)
+		if err != nil {
+			log.Print("json format err:", err)
+			return
+		}
+		client := &http.Client{}
+		body := bytes.NewBuffer(b)
+		tokenReq, err := http.NewRequest("POST", PANO_TOKEN_URL, body)
+		if err != nil {
+			log.Print("get token from pano cloud err:", err)
+			return
+		}
+		tokenReq.Header.Set("Content-Type", "application/json")
+		tokenReq.Header.Set("Authorization", "PanoSign "+generatePanoSign(req.AppId, APP_SECRET))
+		rand.Seed(time.Now().UnixNano())
+		tokenReq.Header.Set("Tracking-Id", string(rand.Intn(100000)))
+
+		tokenRes, err := client.Do(tokenReq)
+		defer tokenRes.Body.Close()
+		content, err := ioutil.ReadAll(tokenRes.Body)
+		if err != nil {
+			log.Print("read pano token err:", err)
+			return
+		}
+		var sdkTokenRes = &SdkToken{}
+		err = json.Unmarshal(content, sdkTokenRes)
+		c.String(http.StatusOK, sdkTokenRes.Token)
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080 (for "localhost:8080")
+}
+
+func signature(appId string, timestamp int64, appSecret string) string {
+	mac := hmac.New(sha256.New, []byte(appSecret))
+	mac.Write([]byte(fmt.Sprintf("%s%d", appId, timestamp)))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func generatePanoSign(appId string, appSecret string) string {
+	current := time.Now().Unix()
+	return fmt.Sprintf("%s.%d.%s", appId, current, signature(appId, current, appSecret))
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # BasicAppServer
-Basic App Server for BasicVideoCall to get paon token
+Basic App Server for BasicVideoCall to get pano token
+
+这是一个简单的App Server 示例代码(Go/Java)，可以和[BasicVideoCall](https://github.com/PanoVideo/video-call-samples.git) 搭配使用,完成基本上音视频功能
+## 第一步 
+将自己的appSecret 填入到该应用的 **APP_SECRET** 变量中，启动该应用。
+## 第二步
+将相关参数如appId/channelId等参数填入BasicVideoCall，然后请求该App Server, 获取token。
+
+启动该示例代码之后，你可以像这样发出类似请求
+```shell script
+curl --location --request POST 'http://localhost:8080/app/login' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "appId": "xxx", 
+    "userId": "20190822",
+    "channelId": "20190822",
+    "duration": 800,
+    "privileges": 0,
+    "channelDuration": 12
+}'
+```
+
+## 第三步
+通过token完成和Pano Cloud的音视频交互
+
+Tips
+>1.请先到https://console.pano.video 注册登录
+>2.在应用管理中创建自己的应用
+>3.将appId和appSecret 放到该示例代码中
+
+---
+完整的文档，请访问拍乐云 [文档中心](https://developer.pano.video/getting-started/intro/)
